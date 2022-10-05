151c7924aadc59ecae7aec2d6ba67d4580fcb46e
samuelc7 <cummings.samuel007@gmail.com>
2022-10-05 15:56:58 -0600

    updated the access control to allow front-end site to work

--- lesson1/app.js
+++ lesson1/app.js
@@ -6,7 +6,6 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
-// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 
 const connection = require("./DB/Connection");
@@ -16,7 +15,16 @@ DB.connectDB();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Request-With, Content-Type, Accept, Z-key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 app.use('/', require('./routes'));
 
 app.listen(port, () => {
